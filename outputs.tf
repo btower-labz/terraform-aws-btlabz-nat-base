53ba97edfaf647b6add2da7a7b2c03468d77c58d
BTower Labz <labz@btower.net>
2018-01-21 20:30:53 +0000

    ...

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "public-ip" {
-  value = "${aws_eip.main.public_ip}"
+  description = "NAT's public IP address."
+  value       = "${aws_eip.main.public_ip}"
 }
 
 output "nat-id" {
-  value = "${aws_nat_gateway.main.id}"
+  description = "NAT identifier."
+  value       = "${aws_nat_gateway.main.id}"
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 variable "name" {
-  type    = "string"
-  default = "main-vpc"
+  description = "NAT name. Will be used as the 'Name' tag value."
+  type        = "string"
+  default     = "main-vpc"
 }
 
 variable "subnet-id" {
-  type = "string"
+  description = "Public subnet identifier to place NAT into."
+  type        = "string"
 }
 
 variable "tags" {
-  type    = "map"
-  default = {}
+  description = "Additional tags."
+  type        = "map"
+  default     = {}
 }
