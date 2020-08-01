ffa5dada4f0abed972c3bcb37269f89974d97928
BTower Labz <labz@btower.net>
2020-08-01 14:26:37 +0000

    updated to 12x

--- variables.tf
+++ variables.tf
@@ -1,16 +1,17 @@
 variable "name" {
   description = "NAT name. Will be used as the 'Name' tag value."
-  type        = "string"
+  type        = string
   default     = "main-vpc"
 }
 
 variable "subnet_id" {
   description = "Public subnet identifier to place NAT into."
-  type        = "string"
+  type        = string
 }
 
 variable "tags" {
   description = "Additional tags."
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
+
